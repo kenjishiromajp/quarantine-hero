dd8a5371a5bb36ae01de626c5a559ba11fd641b4
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2020-03-21 01:09:21 +1000

    feat(next): setting up

---
+++ next.config.js
@@ -0,0 +1,59 @@
+const withLess = require('@zeit/next-less');
+const resolve = require('resolve');
+
+// styled jsx will fail without it
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {}
+}
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    // theme antd here
+    modifyVars: {'@primary-color': '#FF9933'}
+  },
+  webpack: (config, { defaultLoaders, dir, isServer }) => {
+    // Probably a better way to do this: https://github.com/nuxt-community/modules/issues/98#issuecomment-318736546
+    if (defaultLoaders.babel.options.plugins === undefined) {
+      defaultLoaders.babel.options.plugins = []
+    }
+    defaultLoaders.babel.options.plugins.push(['import', {
+      'libraryName': 'antd',
+      'style': true
+    }]);
+
+    config.externals = [];
+
+    if (isServer) {
+
+      // add antd to https://github.com/zeit/next.js/blob/canary/build/webpack.js#L34-L59
+      // This makes sure paths are relative when pushing build to other systems
+      config.externals.push((context, request, callback) => {
+        resolve(request, { basedir: dir, preserveSymlinks: true }, (err, res) => {
+          if (err) {
+            return callback();
+          }
+
+          // Default pages have to be transpiled
+          if (res.match(/node_modules[/\\]next[/\\]dist[/\\]pages/)) {
+            return callback();
+          }
+
+          // Webpack itself has to be compiled because it doesn't always use module relative paths
+          if (res.match(/node_modules[/\\]webpack/) || res.match(/node_modules[/\\]css-loader/) || res.match(/node_modules[/\\]antd/)) {
+            return callback();
+          }
+
+          if (res.match(/node_modules[/\\].*\.js$/)) {
+            return callback(null, `commonjs ${request}`)
+          }
+
+          callback()
+        })
+      })
+
+    }
+
+    return config
+  },
+})
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { Button, Col, Row } from 'antd';
 
 const Home = () => (
   <div className="container">
@@ -8,196 +9,34 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
+      <section className="main orange">
+        <Row justify="center" align="center">
+          <Col span={24}>
+            <h1>
+              <span className="text-orange">Do you</span> need something<span className="text-orange">during the</span> quarantine period?
+            </h1>
+            <h2><span className="text-orange">You can ask for helpto</span> quarantine heroes!</h2>
+          </Col>
           <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
+            Website to help people who are vulnerable to the virus that cannot go out because of quarantine, you can pick up one or more help posted or you can ask for help in simple stuffs
           </p>
-        </a>
-      </div>
+        </Row>
+        <Row justify="center" align="center">
+          <Button>I need a quarantine hero</Button>
+          <Button>I want to be a quarantine hero</Button>
+        </Row>
+      </section>
+
+      <section>
+        <p>
+          During this time of quarantine, there are some people who are more likely to contract the virus and will be more affected by the symptoms.
+        </p>
+        <p>
+          So even simple tasks such as walking around with a dog or buying groceries, or transporting the children can be a problem for them.
+        </p>
+      </section>
     </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
